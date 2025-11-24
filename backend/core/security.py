88c3a35b0673df9c2f0a85b914188b66bfa7f0b8
wldy4627 <wldy4627@gmail.com>
2025-11-25 00:13:44 +0900

    feat/#5: 로그인 API

--- backend/core/security.py
+++ backend/core/security.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 from passlib.context import CryptContext
+from jose import jwt
+
 # 설정 (나중에 .env로)
 SECRET_KEY = "planb-secret-key-change-me" # 아무 문자열이나 길게
 ALGORITHM = "HS256"
@@ -10,3 +12,15 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # 비밀번호 해싱 (암호화)
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
+# 비밀번호 검증
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+# JWT 토큰 생성
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
