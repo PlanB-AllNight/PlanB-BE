0a0cd79985186656706c640a5e9cc1748900bd8e
wldy4627 <wldy4627@gmail.com>
2025-11-25 00:13:44 +0900

    feat/#5: 로그인 API

--- backend/api/user.py
+++ backend/api/user.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session, select
 from backend.database import get_session
-from backend.models.user import User, UserCreate, UserRead
-from backend.core.security import get_password_hash
+from backend.models.user import User, UserCreate, UserLogin, UserRead
+from backend.core.security import get_password_hash, verify_password, create_access_token
 
 router = APIRouter()
 
@@ -31,3 +31,24 @@ def signup(user: UserCreate, session: Session = Depends(get_session)):
     session.refresh(db_user)
     
     return db_user
+
+# 로그인
+@router.post("/login")
+def login(user: UserLogin, session: Session = Depends(get_session)):
+    db_user = session.exec(select(User).where(User.userId == user.userId)).first()
+    
+    if not db_user or not verify_password(user.password, db_user.password):
+        raise HTTPException(status_code=401, detail="이메일 또는 비밀번호가 틀렸습니다.")
+    
+    # 토큰 발급
+    access_token = create_access_token(data={"sub": str(db_user.id)})
+    
+    return {
+        "access_token": access_token, 
+        "token_type": "bearer",
+        "user": {
+            "id": db_user.id,
+            "userId": db_user.userId,
+            "name": db_user.name
+        }
+    }
